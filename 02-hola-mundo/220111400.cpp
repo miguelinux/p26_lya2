ae4f511361db9b91225e103a7558591d32d004af
Carlos Santiago Ramirez Sandoval <screamoon668@gmail.com>
2026-01-29 15:40:53 -0600

    NN

--- 02-hola-mundo/220111400.cpp
+++ 02-hola-mundo/220111400.cpp
@@ -1,8 +1,3 @@
- /*
- * Copyright (C) <year> <name of author>
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
 
 #include <iostream>
 
@@ -14,15 +9,4 @@ int main (int argc, char *argv[])
 	return 0;
 }
 
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: t
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 noexpandtab:
- * :indentSize=8:tabSize=8:noTabs=false:
- */
+
