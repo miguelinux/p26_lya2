b33d79a8bb45a4b0844daa4f82dfb002f1c33978
Jair Jecsan Flores Rodriguez <za220120146@zapopan.tecmm.edu.mx>
2026-02-10 20:09:27 -0600

    equipo

---
+++ 05-entero_flotante/equipo.cpp
@@ -0,0 +1,102 @@
+/*FLORES RODRIGUEZ JAIR JECSAN
+SOLIS MORALES CARLOS ALBERTO
+CASTRO MELGOZA KATIA LIZBETH
+OROZCO LOPEZ JUAN ESTEBAN
+CORREA CRUZALEI HECTOR FERNANDO*/
+#include <iostream>
+#include <string>
+#include "estados.h"
+
+using namespace std;
+
+int analiza(string str)
+{
+    int estado = E_INICIAL;
+
+    for (char c : str) {
+        switch (c) {
+            case '+':
+            case '-':
+                if (estado == E_INICIAL) {
+                    estado = E_SIGNO;
+                }
+                else if (estado == E_NOTA_CIEN) {
+                    estado = E_NOTA_CIEN;
+                }
+                else {
+                    estado = E_ERROR;
+                }
+                break;
+
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                switch (estado) {
+                    case E_INICIAL:
+                    case E_SIGNO:
+                    case E_ENTERO:
+                        estado = E_ENTERO;
+                        break;
+                    case E_FLOTANTE:
+                        estado = E_FLOTANTE;
+                        break;
+                    case E_NOTA_CIEN:
+                        estado = E_NOTA_CIEN;
+                        break;
+                    default:
+                        estado = E_ERROR;
+                }
+                break;
+
+
+            case '.':
+                if (estado == E_ENTERO) {
+                    estado = E_FLOTANTE;
+                } else {
+                    estado = E_ERROR;
+                }
+                break;
+
+            case 'e':
+            case 'E':
+                if (estado == E_ENTERO || estado == E_FLOTANTE) {
+                    estado = E_NOTA_CIEN;
+                } else {
+                    estado = E_ERROR;
+                }
+                break;
+
+
+            default:
+                estado = E_ERROR;
+                break;
+        }
+        if (estado == E_ERROR) break;
+    }
+    return estado;
+}
+
+int main(int argc, char *argv[]) {
+    int estado_final = E_ERROR;
+    string cadena;
+
+    cout << "Ingresa la cadena a analizar: ";
+    cin >> cadena;
+
+    estado_final = analiza(cadena);
+
+    switch(estado_final) {
+        case E_ENTERO:
+            cout << "Es un entero" << endl;
+            break;
+        case E_FLOTANTE:
+            cout << "Es un flotante" << endl;
+            break;
+        case E_NOTA_CIEN:
+            cout << "Es un exponencial" << endl;
+            break;
+        default:
+            cout << "Error: no se identifico la entrada" << endl;
+    }
+    return 0;
+}
+
