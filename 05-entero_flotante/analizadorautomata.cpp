3c25d164b552b2510f0dcd11d4e97f1c311b9d84
Omar Ulises Hernandez Perez <omar_100604@hotmail.com>
2026-02-12 17:15:32 -0600

    correcion

--- 05-entero_flotante/analizadorautomata.cpp
+++ 05-entero_flotante/analizadorautomata.cpp
@@ -1,97 +1,125 @@
+/*
+Graciela Mora Esparza 210111853
+Carlos Santiago Ramírez Sandoval 220111400
+Omar Ulises Hernandez Perez 220111901
 
+*/
 #include <iostream>
 #include <string>
-
+#include <cctype>
 #include "estados.h"
 
 using namespace std;
 
 int analiza(string str)
 {
-	int estado = 0;
+    int estado = E_INICIAL;
 
-	for (char c : str) {
-		switch (c) {
-			case '+':
-			case '-':
-				if (estado == E_INICIAL || estado == E_NOTA_CIEN) {
-					estado = E_SIGNO;	
-				}
-				else {
-					estado = E_ERROR;
-				}
-				break;
-			case '.':
-				if(estado==E_ENTERO)
-				estado= E_FLOTANTE;	
-				else
-				estado=	E_ERROR;
-				break;
-			case'e':
-			case 'E':
-				if(estado== E_ENTERO || estado== E_FLOTANTE)
-					estado=E_NOTA_CIEN;
-				else
-					estado=E_ERROR;
-				break;
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        char c = str[i];
 
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				switch (estado) {
-					case E_INICIAL:
-					case E_SIGNO:
-					case E_ENTERO:
-						estado = E_ENTERO;
-						break;
-					case E_FLOTANTE:
-						estado=E_FLOTANTE;
-						break;
-					 case E_NOTA_CIEN:
-                                                estado=E_NOTA_CIEN;
-                                        	break;
-					default:
-						estado = E_ERROR;
-				}
-				break;
-			default:
-				estado=E_ERROR;
-		}
-	if(estado == E_ERROR)
-	break;
+        switch (estado)
+        {
+        case E_INICIAL:
+            if (c == '+' || c == '-')
+                estado = E_SIGNO;
+            else if (isdigit(c))
+                estado = E_ENTERO;
+            else
+                return E_ERROR;
+            break;
 
-	}
-	return estado;
+        case E_SIGNO:
+            if (isdigit(c))
+                estado = E_ENTERO;
+            else
+                return E_ERROR;
+            break;
+
+        case E_ENTERO:
+            if (isdigit(c))
+                estado = E_ENTERO;
+            else if (c == '.')
+                estado = E_FLOTANTE;
+            else if (c == 'e' || c == 'E')
+                estado = E_NOTA_CIEN;
+            else
+                return E_ERROR;
+            break;
+
+        case E_FLOTANTE:
+            if (isdigit(c))
+                estado = E_FLOTANTE;
+            else if (c == 'e' || c == 'E')
+                estado = E_NOTA_CIEN;
+            else
+                return E_ERROR;
+            break;
+
+        case E_NOTA_CIEN:
+            if (c == '+' || c == '-')
+                estado = 100; // estado interno para signo exponente
+            else if (isdigit(c))
+                estado = 200; // estado interno para dígitos exponente
+            else
+                return E_ERROR;
+            break;
+
+        case 100: // signo del exponente
+            if (isdigit(c))
+                estado = 200;
+            else
+                return E_ERROR;
+            break;
+
+        case 200: // dígitos del exponente
+            if (isdigit(c))
+                estado = 200;
+            else
+                return E_ERROR;
+            break;
+        }
+    }
+
+    // Estados finales válidos
+    if (estado == E_ENTERO)
+        return E_ENTERO;
+
+    if (estado == E_FLOTANTE)
+        return E_FLOTANTE;
+
+    if (estado == 200)  // terminó correctamente el exponente
+        return E_NOTA_CIEN;
+
+    return E_ERROR;
 }
 
-int main (int argc, char *argv[])
+int main()
 {
-	int estado_final = E_ERROR;
-	string cadena;
-	cin >> cadena;
+    string cadena;
+    cin >> cadena;
 
-	estado_final = analiza(cadena);
+    int resultado = analiza(cadena);
 
-	switch(estado_final) {
-		case E_ENTERO:
-			cout << "Es un entero" << endl;
-			break;
-		case E_FLOTANTE:
-			cout << "Es un flotante" << endl;
-			break;
-		case E_NOTA_CIEN:
-			cout << "Es un flotante en notacion cientifica" << endl;
-			break;
-		default:
-			cout << "Error: no se identifico la entrada" << endl;
-	}
+    switch (resultado)
+    {
+    case E_ENTERO:
+        cout << "Es un entero" << endl;
+        break;
 
-	return 0;
+    case E_FLOTANTE:
+        cout << "Es un flotante" << endl;
+        break;
+
+    case E_NOTA_CIEN:
+        cout << "Es notacion cientifica" << endl;
+        break;
+
+    default:
+        cout << "Error" << endl;
+    }
+
+    return 0;
 }
+
