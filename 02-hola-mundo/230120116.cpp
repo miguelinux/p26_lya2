7a16f3d5f96d4013720742632e497a3e170bcf24
Diego Javier De Avila Alvarez <za230120116@zapopan.tecmm.edu.mx>
2026-02-25 17:47:16 -0600

    Hola mundo

--- 02-hola-mundo/230120116.cpp
+++ 02-hola-mundo/230120116.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "230120116" << endl;
-	cout << "Diego Javier De Avila Alvarez" << endl;
+	cout << "Diego Javier De Avila Alvarez." << endl;
 
 	return 0;
 }
