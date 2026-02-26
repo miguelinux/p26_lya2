441317f6877f0bea42b1c94860d539686cc08b6f
Daniela Ivette Nava Miranda <za220111414@zapopan.tecmm.edu.mx>
2026-02-25 21:52:50 -0600

    Añadiendo nuestro automata de identificadores y p. reservadas

---
+++ 06-identificadores_y_p_reservadas/AutomataNinoxit.cpp
@@ -0,0 +1,77 @@
+#include "AutomataNinoxit.h"
+
+const std::string reservadas = "const|int|double|float|if|else|switch|case|default|for|while|do|main";
+const std::string numeros = "1234567890";
+const std::string letras = "abcdefghijklmnopqrstuvwxyz";
+
+bool checker(const std::string& word, const std::string base) {
+    if (word.empty()) return false;
+    return (base.find(word) != std::string::npos);
+}
+
+int analizador(const std::string& cadena) {
+    int estado = E_INICIAL;
+
+    if (cadena.empty()) return E_ERROR;
+    
+    for (char ch : cadena) {
+        const std::string _ch(1, ch);
+
+        if (checker(_ch, numeros)) {
+            switch (estado) {
+                case E_INICIAL:
+                case E_SIGNO:
+                case E_ENTERO:
+                    estado = E_ENTERO;
+                    break;
+                case E_PUNTO:
+                case E_FLOTANTE:
+                    estado = E_FLOTANTE;
+                    break;
+                case E_NOTA_CIEN:
+                    estado = E_NOTA_CIEN;
+                    break;
+                case E_IDENTIFICADOR:
+                    estado = E_IDENTIFICADOR;
+                    break;
+                default:
+                    estado = E_ERROR;
+            }
+        }
+        else if (checker(_ch, letras)) {
+            switch (estado) {
+                case E_INICIAL:
+                case E_IDENTIFICADOR:
+                    estado = E_IDENTIFICADOR;
+                    break;
+                default:
+                    estado = E_ERROR;
+            }
+        }
+        else if (ch == '.') {
+            if (estado == E_ENTERO) {
+                estado = E_PUNTO;
+            } else {
+                estado = E_ERROR;
+            }
+        }
+        else if (ch == '+' || ch == '-') {
+            if (estado == E_INICIAL) {
+                estado = E_SIGNO;
+            } else {
+                estado = E_ERROR;
+            }
+        }
+        else {
+            estado = E_ERROR;
+        }
+
+        if (estado == E_ERROR) break;
+    }
+   
+    if (estado == E_IDENTIFICADOR && checker(cadena, reservadas)) {
+        estado = E_RESERVADA;
+    }
+    
+    return estado;
+}
