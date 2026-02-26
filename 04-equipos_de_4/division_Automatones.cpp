d65a5fdcb18c30a9903d713a1619dcfe7aca33f3
Omar Ulises Hernandez Perez <omar_100604@hotmail.com>
2026-02-25 21:33:17 -0600

    Se agregan automatones de suma, resta, multiplicacion y division

---
+++ 04-equipos_de_4/division_Automatones.cpp
@@ -0,0 +1,12 @@
+#include "division_Automatones.h"
+
+
+float division(float num1,float  num2){
+	float resultado=0;
+	if (num2!=0){
+	resultado=num1/num2;
+	}else{
+	resultado=0;
+	}
+	return resultado;
+}
---
+++ 04-equipos_de_4/division_Automatones.h
@@ -0,0 +1,8 @@
+#ifndef DIVISION_AUTOMATONES_H
+#define DIVISION_AUTOMATONES_H
+
+float division(float num1, float num2);
+
+#endif
+
+
