1037a05c7ecaf429d039cf174bbb3a5953dafc03
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2026-02-26 15:44:09 -0600

    iniciando archivos en c++

---
+++ 07-archivos_en_cpp/leer_archivo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	if (argc < 2) {
+		cerr << "Error: no hay suficientes argumentos" << endl;
+	}
+	return 0;
+}
+
+// vi: set shiftwidth=8 tabstop=8 noexpandtab:
