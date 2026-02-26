eccc75d39e45085b0a38296f32a8dfd5f9483c09
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2026-02-26 15:47:28 -0600

    archivos 2

--- 07-archivos_en_cpp/leer_archivo.cpp
+++ 07-archivos_en_cpp/leer_archivo.cpp
@@ -6,6 +6,10 @@ int main (int argc, char *argv[])
 {
 	if (argc < 2) {
 		cerr << "Error: no hay suficientes argumentos" << endl;
+		return 1;
+	}
+	for (int i=0; i<argc; i++) {
+		cout << i << ": " << argv[i] << endl;
 	}
 	return 0;
 }
