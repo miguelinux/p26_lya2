ca2b475b8b2646801d6ef46a2aa72257a8717bc3
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2026-03-18 16:10:51 -0600

    ejemplo de los estados de compilacion

---
+++ 08-fases_de_un_compilador/constantes.h
@@ -0,0 +1,8 @@
+#ifndef _CONSTANTES_H_
+#define _CONSTANTES_H_
+
+#define UNO  1
+#define DOS  2
+#define TRES 3
+
+#endif
---
+++ 08-fases_de_un_compilador/ejemplo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "constantes.h"
+
+int main (int argc, char *argv[])
+{
+	std::cout << UNO << std::endl;
+	std::cout << DOS << std::endl;
+	std::cout << TRES << std::endl;
+}
